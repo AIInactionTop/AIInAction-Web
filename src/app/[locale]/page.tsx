33a591940e45e800914a40d0105138b6a78886ec
Jason <superjavason@gmail.com>
2026-03-03 23:03:35 +0800

    add cs chatbox

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { getStats, getCategories, getChallenges } from "@/lib/challenges";
 import { HomeClient } from "./home-client";
 import { setRequestLocale } from "next-intl/server";
@@ -17,10 +18,28 @@ export default async function HomePage({ params }: Props) {
   ]);
 
   return (
-    <HomeClient
-      stats={stats}
-      categories={JSON.parse(JSON.stringify(categories))}
-      featured={JSON.parse(JSON.stringify(featured))}
-    />
+    <>
+      <HomeClient
+        stats={stats}
+        categories={JSON.parse(JSON.stringify(categories))}
+        featured={JSON.parse(JSON.stringify(featured))}
+      />
+      <Script
+        id="tawk-to"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69a6ee8d1a02b31c39abc381/1jiq17mh7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`,
+        }}
+      />
+    </>
   );
 }
